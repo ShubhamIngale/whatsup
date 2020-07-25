2697844222892f607c3f5e3bcfbddd473fc88998
ShubhamFistbumpDigital <shubham.fistbump@gmail.com>
2020-07-25 22:00:23 +0530

    notification

--- public/js/main.js
+++ public/js/main.js
@@ -26,9 +26,10 @@ socket.on('roomUsers', ({room, users}) => {
 socket.on('message', message => {
         console.log(message);
         outputMessage(message);
-        if(!document.hasFocus()) {
-                notify(message);
-            }
+        // if(!document.hasFocus()) {
+        //         notify(message);
+        //     }
+        notify(message);
         // scroll down
         chatMessages.scrollTop = chatMessages.scrollHeight;
 });
